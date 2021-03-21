5a99c30ce359dd87ecc9b15a1755a1f024522693
Michael-12211 <michaeldavidkempe@gmail.com>
2021-03-21 19:41:22 -0400

    Updated login page to look better. Added text fields to enter username and password. Made login page a stateful widget.

--- lib/main.dart
+++ lib/main.dart
@@ -5,43 +5,109 @@ import 'package:schedu_block/signup.dart';
 void main() {
   runApp(MaterialApp(
     title: 'Schedu-block',
-    home: Login()
+    home: LoginPage()
   ));
 }
 
-class Login extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<LoginPage> {
+
+  TextEditingController usernameController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Image(image: AssetImage('images/Logo.PNG')),
-            ElevatedButton(
-                onPressed: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-                  );
-                },
-                child: Text('Enter')
-            ),
-            ElevatedButton(
-                onPressed: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SignUp()),
-                  );
-                },
-                child: Text('Sign up')
+    final usernameField = TextField(
+      controller: usernameController,
+      obscureText: false,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Username",
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))
+      )
+    );
+    final passwordField = TextField(
+      controller: passwordController,
+        obscureText: true,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+            hintText: "Password",
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))
+        )
+    );
+    final loginButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        onPressed: () {
+          print('The user is ${usernameController.text} with password ${passwordController.text}');
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
+          );
+        },
+        child: Text("Login",
+          textAlign: TextAlign.center,
+        )
+      )
+    );
+    final signUpButton = Material(
+        elevation: 5.0,
+        borderRadius: BorderRadius.circular(30),
+        color: Color(0xff01A0C7),
+        child: MaterialButton(
+            minWidth: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SignUp()),
+              );
+            },
+            child: Text("Sign up",
+              textAlign: TextAlign.center,
             )
-          ],
+        )
+    );
+
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 155,
+                  child: Image(image: AssetImage('images/Logo.PNG'))
+                ),
+                SizedBox(height: 45),
+                usernameField,
+                SizedBox(height: 15),
+                passwordField,
+                SizedBox(height:15),
+                loginButton,
+                SizedBox(height: 15),
+                signUpButton
+              ],
+            )
+
+        )
         )
       )
     );
   }
 }
-
