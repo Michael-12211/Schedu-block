42e8ab08162b2c0c36f4ec48119b7a50b22108f3
Michael-12211 <michaeldavidkempe@gmail.com>
2021-04-01 19:25:24 -0400

    Significant progress has been made in loading data from firebase. What I am about to do will most likely break everything, so I am using this as a backup.

--- lib/schedules.dart
+++ lib/schedules.dart
@@ -1,14 +1,67 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:schedu_block/dailyview.dart';
+import 'package:firebase_database/firebase_database.dart';
 
-class Schedules extends StatelessWidget {
+class Schedules extends StatefulWidget {
+  Schedules({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _SchedulesState createState() => _SchedulesState();
+}
+
+class _SchedulesState extends State<Schedules> {
+
+  final database = FirebaseDatabase.instance.reference();
+
+  var schedules = <Widget>[];
+
   @override
   Widget build(BuildContext context) {
 
-    //making all the schedules show up uniform
-    final schedules = <Widget>[];
-    for (var i = 0; i < 10; i++){
-      schedules.add( MaterialButton(
+    var db = database;
+    print ('this is ' + db.toString());
+
+    /*database.once().then((DataSnapshot snapshot) {
+      //print ('Data : ${snapshot.value}');
+      //var parsed = jsonDecode(snapshot);
+      var map = snapshot.value as Map<dynamic, dynamic>;
+      //print ('first ' + map['users'].toString());
+      var scheds = map['users']['admin']['schedules'];
+      //print ('schedules: ' + scheds.toString());
+      //String name = scheds['example']['name'];
+      //print (name);
+
+      scheds.forEach((key, value) {
+        print ('the schedule is: ' + value['name']);
+        schedules.add( MaterialButton(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 90,
+                  width: 60,
+                  child: Image(image: AssetImage('images/Logo.PNG')),
+                ),
+                Text(value['name'])
+              ],
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DailyView(oName: value['name'])),
+              );
+            }
+        ));
+      });
+    });*/
+
+    //sleep(Duration(seconds: 3));
+
+    /*for (var i = 0; i < 9; i++){
+      MaterialButton m = ( MaterialButton(
         child: Column(
           children: [
             SizedBox(
@@ -26,8 +79,11 @@ class Schedules extends StatelessWidget {
           );
         }
       ));
-    }
+      //var m = Text("test");
+      schedules.add(m);
+    }*/
 
+    print ('making scaffold');
     return Scaffold(
         appBar: AppBar(
             title: Text('Schedules')
